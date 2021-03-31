6409fc7174c20486b584d95a84fb46663c4811d4
sairamprasadbandi <76676982+sairamprasadbandi@users.noreply.github.com>
2021-03-31 14:31:03 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -1,6 +1,9 @@
-        num1 = 1.5
-        num2 = 6.3
+# Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
 
-        # Add two numbers
-        sum = num1 + num2
-        return sum
+# Add two numbers
+sum = float(num1) + float(num2)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
